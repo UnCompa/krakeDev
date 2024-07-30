fd391d17a26945048a51e61724ba695cde4a006a
UnCompa <brandonddxd@gmail.com>
2024-07-30 14:23:30 -0500

    funcion recuperar int y float

--- UTILS/saludo.js
+++ UTILS/saludo.js
@@ -3,8 +3,20 @@ const recuperarTexto = (idComponente) => {
   let valor = componente.value
   return valor
 }
+const recuperarInt = (id) => {
+  let valor = recuperarTexto(id)
+  let valorEntero = parseInt(valor)
+  return valorEntero
+}
+const recuperarFloat = (id) => {
+  let valor = recuperarTexto(id)
+  let valorEntero = parseFloat(valor)
+  return valorEntero
+}
 
 const saludar = () => {
-  let nombre = recuperarTexto("txtTexto")
-  let apellido = recuperarTexto("txtTexto")
+  let nombre = recuperarTexto("txtNombre")
+  let apellido = recuperarTexto("txtApellido")
+  let edad = recuperarInt("txtEdad")
+  let estatura = recuperarFloat("txtEstatura")
 }
