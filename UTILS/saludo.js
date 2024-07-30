c404c288325cdff09156c5595a6587a1ab76e3a4
UnCompa <brandonddxd@gmail.com>
2024-07-30 14:35:04 -0500

    mostrar texto en caja

--- UTILS/saludo.js
+++ UTILS/saludo.js
@@ -13,10 +13,26 @@ const recuperarFloat = (id) => {
   let valorEntero = parseFloat(valor)
   return valorEntero
 }
-
+const mostrarTexto = (idComponente,mensaje) => {
+  let componente = document.getElementById(idComponente)
+  componente.innerText = mensaje
+}
+const mostrarImagen = (idComponente,rutaImagen) => {
+  let componente = document.getElementById(idComponente)
+  componente.src = rutaImagen
+}
+const mostrarTextoEnCaja = (idComponente,mensaje) => {
+  let componente = document.getElementById(idComponente)
+  componente.value = mensaje
+}
 const saludar = () => {
   let nombre = recuperarTexto("txtNombre")
   let apellido = recuperarTexto("txtApellido")
   let edad = recuperarInt("txtEdad")
   let estatura = recuperarFloat("txtEstatura")
+
+  let mensajeBienenida = `Bienvenido ${nombre} ${apellido}`
+  mostrarTexto("lblResultado", mensajeBienenida)
+  mostrarImagen("imgSaludo", "./imgs/98855619a570b9cc42a3494f418cf22d.gif")
+  mostrarTextoEnCaja("txtNombre", "")
 }
