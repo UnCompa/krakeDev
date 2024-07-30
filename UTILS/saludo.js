0834bc09608f0053d43cd9d8252d111c502a4a40
UnCompa <brandonddxd@gmail.com>
2024-07-30 14:38:11 -0500

    creacion de utilitarios.js

--- UTILS/saludo.js
+++ UTILS/saludo.js
@@ -1,30 +1,3 @@
-const recuperarTexto = (idComponente) => {
-  let componente = document.getElementById(idComponente)
-  let valor = componente.value
-  return valor
-}
-const recuperarInt = (id) => {
-  let valor = recuperarTexto(id)
-  let valorEntero = parseInt(valor)
-  return valorEntero
-}
-const recuperarFloat = (id) => {
-  let valor = recuperarTexto(id)
-  let valorEntero = parseFloat(valor)
-  return valorEntero
-}
-const mostrarTexto = (idComponente,mensaje) => {
-  let componente = document.getElementById(idComponente)
-  componente.innerText = mensaje
-}
-const mostrarImagen = (idComponente,rutaImagen) => {
-  let componente = document.getElementById(idComponente)
-  componente.src = rutaImagen
-}
-const mostrarTextoEnCaja = (idComponente,mensaje) => {
-  let componente = document.getElementById(idComponente)
-  componente.value = mensaje
-}
 const saludar = () => {
   let nombre = recuperarTexto("txtNombre")
   let apellido = recuperarTexto("txtApellido")
