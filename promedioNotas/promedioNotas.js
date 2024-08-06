db64d1cad454ff0455faf1bf419ad9963cd75da6
UnCompa <brandonddxd@gmail.com>
2024-08-06 10:47:49 -0500

    Promedio Notas v2 con comprobaciones

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -3,11 +3,21 @@ const calcularPromedioNotas = () => {
   const nota2 = recuperarFlotante("txtNota2");
   const nota3 = recuperarFlotante("txtNota3");
   const promedio = calcularPromedio(nota1, nota2, nota3);
-  cambiarTexto("lblResultado", promedio.toFixed(2));
-  if (promedio > 7 ) {
-    cambiarImagen("imgResultado", "./Exito.gif")
+  if (promedio < 5 && promedio > 0) {
+    cambiarTexto("lblMensaje", "- REPROBADO");
+    cambiarTexto("lblResultado", promedio.toFixed(2));
+    cambiarImagen("imgResultado", "./Fracaso.gif");
+  } else if (promedio >= 5 && promedio <= 8) {
+    cambiarTexto("lblMensaje", "- Buen trabajo");
+    cambiarTexto("lblResultado", promedio.toFixed(2));
+    cambiarImagen("imgResultado", "./Good.gif");
+  } else if (promedio > 8 && promedio <= 10) {
+    cambiarTexto("lblMensaje", "- EXCELENTE");
+    cambiarTexto("lblResultado", promedio.toFixed(2));
+    cambiarImagen("imgResultado", "./Exito.gif");
   } else {
-    cambiarImagen("imgResultado", "./Fracaso.gif")
-
+    cambiarTexto("lblMensaje", "- DATOS INCORRECTOS");
+    cambiarTexto("lblResultado", promedio.toFixed(2));
+    cambiarImagen("imgResultado", "./SinDatos.gif");
   }
 };
