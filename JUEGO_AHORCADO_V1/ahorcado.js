9315bb0c675bfaf0f69dcaa8e977c034feb4498f
UnCompa <brandonddxd@gmail.com>
2024-08-10 15:02:02 -0500

    Ejercicio 40 - Paso 7

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -59,10 +59,10 @@ const ingreseLetra = () => {
   if (esMayuscula(letra)) {
     validar(letra);
     if (coincidencias == 5) {
-      alert("Ha ganado");
+      mostrarImagen("ahorcadoImagen", "./ganador.gif");
     }
     if (intentos == 10) {
-      alert("Ha perdido");
+      mostrarImagen("ahorcadoImagen", "./gameOver.gif");
     }
   } else {
     alert("SOLO SE ACEPTAN MAYUSCULAS");
