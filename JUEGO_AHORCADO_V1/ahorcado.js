fac584cc91753571307ce4063f90eabc8a2a7d51
UnCompa <brandonddxd@gmail.com>
2024-08-10 14:34:06 -0500

    Ejercicio 40 - Paso 1

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,4 +1,5 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+let palabraSecreta; //Paso 1
 // PASO 0
 const esMayuscula = (caracter) => {
   let codigo = caracter.charCodeAt(0);
@@ -6,5 +7,23 @@ const esMayuscula = (caracter) => {
     return true;
   } else {
     return false;
+  }
+};
+
+//Paso 1
+const guardarPalabra = () => {
+  let sonMayusculas;
+  const secreta = recuperarTexto("txtSecreta");
+  for (let posicion = 0; posicion < secreta.length; posicion++) {
+    if (esMayuscula(secreta.charAt(posicion))) {
+      sonMayusculas = true;
+    } else {
+      sonMayusculas = false;
+    }
+  }
+  if (secreta.length != 5 || !sonMayusculas) {
+    alert("Las 5 palabras deben ser mayusculas");
+  } else {
+    palabraSecreta = secreta
   }
 };
