9260e18caca0564af3bfb63aa323daf43254728c
UnCompa <brandonddxd@gmail.com>
2024-08-11 09:46:08 -0500

    Ejercicio 45 - Parte 2.1

--- roles/roles.js
+++ roles/roles.js
@@ -6,9 +6,40 @@ let empleados = [
     apellido: "Gonzalez",
     sueldo: 900.0,
   },
+  {
+    cedula: "1729097238",
+    nombre: "Brandon",
+    apellido: "Paillacho",
+    sueldo: 500.0,
+  },
 ];
 
+const mostrarEmpleados = () => {
+  const div = document.getElementById("tablaEmpleados");
+  let contenidoTabla = `<table>
+    <tr>
+      <th>CEDULA</th>
+      <th>NOMBRE</th>
+      <th>APELLIDO</th>
+      <th>SUELDO</th>
+    </tr>
+    `;
+  for (let i = 0; i < empleados.length; i++) {
+    let elemento;
+    elemento = empleados[i];
+    contenidoTabla += `
+      <tr>
+      <td>${elemento.cedula}</td>
+      <td>${elemento.nombre}</td>
+      <td>${elemento.apellido}</td>
+      <td>${elemento.sueldo}</td>
+    </tr>`;
+  }
+  div.innerHTML = contenidoTabla;
+};
+
 const mostrarOpcionEmpleado = () => {
+  mostrarEmpleados();
   mostrarComponente("divEmpleado");
   ocultarComponente("divRol");
   ocultarComponente("divResumen");
